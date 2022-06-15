8aa88fec0ec254b42eb8c110e64a7cf4d6aaf242
Esha <eranade@mit.edu>
2022-06-15 10:02:00 -0600

    Personalized website and todos

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,30 @@
-import React, { Component } from 'react';
-  
+import React, { Component } from "react";
+import "./About.css";
+import profile_pic from "../assets/profile_pic.png";
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        {/* <p>Design your About me page </p> */}
+        <div class="split left">
+          <div className="centered">
+            <img
+              className="profile_image"
+              src={profile_pic}
+              alt="Profile Pic"
+            ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Esha Ranade</div>
+            <div className="brief_description">
+              Hi! I am Esha Ranade, a rising junior studying Computer Science at the Massachusetts Institute of Technology.
+            </div>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
